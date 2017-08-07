4cc29024e0472635d5d1ecedb56dce0ba4a21499
joel ivongbe <lomocallmanager@gmail.com>
2017-08-07 04:36:07 -0400

    tested static files

--- lomocall/settings.py
+++ lomocall/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+
+]
 
 # default static files settings for PythonAnywhere.
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
