2a05659cf39f52d7f9fa7a47d7a6e0a7eccc8881
joel ivongbe <lomocallmanager@gmail.com>
2017-08-07 18:31:38 -0400

    changes

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,4 +4,4 @@ from django.views.generic import TemplateView
 
 
 
-urlpatterns=[url(r'^$',TemplateView.as_view(template_name="webapp/index.html"),name="index"),]
+urlpatterns=[url(r'^$',TemplateView.as_view(template_name="webapp/index_new.html"),name="index"),]
