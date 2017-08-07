55bd61084b6d47c09379bf953610d0e05a8ebabe
joel ivongbe <lomocallmanager@gmail.com>
2017-08-07 14:55:55 -0400

    test

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,4 +4,4 @@ from django.views.generic import TemplateView
 
 
 
-urlpatterns=[url(r'^$',TemplateView.as_view(template_name="webapp/home.html"),name="home"),]
+urlpatterns=[url(r'^$',TemplateView.as_view(template_name="webapp/index.html"),name="index"),]
